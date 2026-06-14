8ad79b13ed38b1a730370b5a08d8193504862810
Kaloyan13Tanev <104224203+Kaloyan13Tanev@users.noreply.github.com>
2026-06-14 20:28:33 +0300

    Set up EntryPoint

--- src/main/java/bg/sofia/uni/fmi/mjt/dungeonsonline/server/EntryPoint.java
+++ src/main/java/bg/sofia/uni/fmi/mjt/dungeonsonline/server/EntryPoint.java
@@ -1,10 +1,33 @@
 package bg.sofia.uni.fmi.mjt.dungeonsonline.server;
 
+import bg.sofia.uni.fmi.mjt.dungeonsonline.common.request.MoveRequest;
+import bg.sofia.uni.fmi.mjt.dungeonsonline.common.request.Request;
+import bg.sofia.uni.fmi.mjt.dungeonsonline.server.handler.Handler;
+import bg.sofia.uni.fmi.mjt.dungeonsonline.server.handler.MoveHandler;
+import bg.sofia.uni.fmi.mjt.dungeonsonline.server.player.PlayerIdPool;
+import bg.sofia.uni.fmi.mjt.dungeonsonline.server.registry.ClientConnectionRegistry;
+import bg.sofia.uni.fmi.mjt.dungeonsonline.server.router.Router;
+import bg.sofia.uni.fmi.mjt.dungeonsonline.server.serialization.ResponseSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class EntryPoint {
 
     void main() {
-        RequestRouter requestRouter = new RequestRouter();
-        GameServer gameServer = new GameServer(requestRouter);
+        PlayerIdPool playerIdPool = new PlayerIdPool();
+
+        Map<Class<? extends Request>, ? extends Handler> routes = Map.of(
+                MoveRequest.class, new MoveHandler()
+                // AttackRequest.class, new AttackHandler(),
+                // PickUpRequest.class, new PickUpHandler()
+        );
+        Router router = new Router(routes);
+
+        ResponseSerializer serializer = new ResponseSerializer();
+        ClientConnectionRegistry registry = new ClientConnectionRegistry(new HashMap<>(), serializer);
+
+        GameServer gameServer = new GameServer(playerIdPool, router, registry);
         gameServer.run();
     }
 

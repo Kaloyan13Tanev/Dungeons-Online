96bdd2ebaa9b054c38e1a99155d02c67dbbf9048
Kaloyan13Tanev <104224203+Kaloyan13Tanev@users.noreply.github.com>
2026-06-14 19:19:12 +0300

    Add primitive Response
    Response class serves the purpose of being a message to the Client sent from the Server.

---
+++ src/main/java/bg/sofia/uni/fmi/mjt/dungeonsonline/common/response/Response.java
@@ -0,0 +1,15 @@
+package bg.sofia.uni.fmi.mjt.dungeonsonline.common.response;
+
+public class Response {
+
+    private final String message;
+
+    public Response(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}

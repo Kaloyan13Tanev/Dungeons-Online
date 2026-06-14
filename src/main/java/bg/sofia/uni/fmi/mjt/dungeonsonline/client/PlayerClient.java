586b68b6941846faab17c0365927fff26da0d0b6
Kaloyan13Tanev <104224203+Kaloyan13Tanev@users.noreply.github.com>
2026-06-14 20:29:05 +0300

    Update PlayerClient

--- src/main/java/bg/sofia/uni/fmi/mjt/dungeonsonline/client/PlayerClient.java
+++ src/main/java/bg/sofia/uni/fmi/mjt/dungeonsonline/client/PlayerClient.java
@@ -1,42 +1,35 @@
 package bg.sofia.uni.fmi.mjt.dungeonsonline.client;
 
-import bg.sofia.uni.fmi.mjt.dungeonsonline.common.Request;
-import com.google.gson.Gson;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class PlayerClient {
 
     private static final int SERVER_PORT = 4444;
-    private static final Gson GSON = new Gson();
 
     static void main() {
+
+        System.out.println("Connected to the server");
         try (Socket socket = new Socket("localhost", SERVER_PORT);
-             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              Scanner scanner = new Scanner(System.in)) {
 
-            Thread.currentThread().setName("Echo client thread " + socket.getLocalPort());
+            //TODO: inject the channels
+            Thread receiveChannel = new Thread(new ClientReceiveChannel(reader));
+            receiveChannel.start();
 
-            System.out.println("Connected to the server.");
+            ClientSendChannel sendChannel = new ClientSendChannel(writer, scanner);
+            sendChannel.start();
 
-            while (true) {
-                System.out.print("Enter message: ");
-                String message = scanner.nextLine();
-
-                if ("quit".equals(message)) {
-                    break;
-                }
-
-                System.out.println("Sending message <" + message + "> to the server...");
-
-                Request request = new Request(message, socket.getLocalPort());
-                writer.println(GSON.toJson(request));
-            }
         } catch (IOException e) {
-            throw new RuntimeException("There was a problem with the network communication", e);
+            throw new RuntimeException(e);
         }
+
     }
 }

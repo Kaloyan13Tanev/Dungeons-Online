cd1ea338fcaf9e61896800e0dbb40a3a1fa7802e
Kaloyan13Tanev <104224203+Kaloyan13Tanev@users.noreply.github.com>
2026-06-14 22:17:10 +0300

    Add ClientSendChannel

---
+++ src/main/java/bg/sofia/uni/fmi/mjt/dungeonsonline/client/channel/ClientSendChannel.java
@@ -0,0 +1,33 @@
+package bg.sofia.uni.fmi.mjt.dungeonsonline.client.channel;
+
+import bg.sofia.uni.fmi.mjt.dungeonsonline.client.serialization.RequestSerializer;
+import bg.sofia.uni.fmi.mjt.dungeonsonline.common.request.MoveRequest;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ClientSendChannel {
+
+    private final BufferedWriter writer;
+    private final Scanner scanner;
+    private final RequestSerializer serializer = new RequestSerializer();
+
+    public ClientSendChannel(BufferedWriter writer, Scanner scanner) {
+        this.writer = writer;
+        this.scanner = scanner;
+    }
+
+    public void start() throws IOException {
+        while (true) {
+            System.out.print("Enter message: ");
+            String message = scanner.nextLine();
+            if ("quit".equals(message)) {
+                break;
+            } else if (message.equalsIgnoreCase("move")) {
+                writer.write(serializer.serialize(new MoveRequest()));
+                writer.flush();
+            }
+        }
+    }
+}

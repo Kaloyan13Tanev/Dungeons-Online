3adc5b26d92f2974b3eac343a4bb97d4af098927
Kaloyan13Tanev <104224203+Kaloyan13Tanev@users.noreply.github.com>
2026-08-05 21:50:54 +0300

    Add Terrain enum

---
+++ src/main/java/bg/sofia/uni/fmi/mjt/dungeonsonline/server/engine/map/Terrain.java
@@ -0,0 +1,17 @@
+package bg.sofia.uni.fmi.mjt.dungeonsonline.server.engine.map;
+
+public enum Terrain {
+
+    GROUND(true),
+    OBSTACLE(false);
+
+    private final boolean walkable;
+
+    Terrain(boolean walkable) {
+        this.walkable = walkable;
+    }
+
+    public boolean isWalkable() {
+        return walkable;
+    }
+}

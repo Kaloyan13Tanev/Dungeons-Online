1d22603cf618a63ae4a0c446f032b383e084f208
Kaloyan13Tanev <104224203+Kaloyan13Tanev@users.noreply.github.com>
2026-08-06 15:36:57 +0300

    Refactor Treasure class

--- src/main/java/bg/sofia/uni/fmi/mjt/dungeonsonline/server/engine/treasure/DroppedTreasure.java
+++ src/main/java/bg/sofia/uni/fmi/mjt/dungeonsonline/server/engine/treasure/DroppedTreasure.java
@@ -1,10 +1,11 @@
 package bg.sofia.uni.fmi.mjt.dungeonsonline.server.engine.treasure;
 
+import bg.sofia.uni.fmi.mjt.dungeonsonline.server.engine.Position;
 import bg.sofia.uni.fmi.mjt.dungeonsonline.server.engine.item.Item;
 
 public class DroppedTreasure extends Treasure {
 
-    public DroppedTreasure(Item item) {
-        super(item, 0);
+    public DroppedTreasure(int id, Position position, Item item) {
+        super(id, position, item, 0);
     }
 }
--- src/main/java/bg/sofia/uni/fmi/mjt/dungeonsonline/server/engine/treasure/Treasure.java
+++ src/main/java/bg/sofia/uni/fmi/mjt/dungeonsonline/server/engine/treasure/Treasure.java
@@ -1,21 +1,34 @@
 package bg.sofia.uni.fmi.mjt.dungeonsonline.server.engine.treasure;
 
+import bg.sofia.uni.fmi.mjt.dungeonsonline.server.engine.Position;
 import bg.sofia.uni.fmi.mjt.dungeonsonline.server.engine.item.Item;
 
 public class Treasure {
 
     private static final int PICKUP_EXPERIENCE = 20;
 
+    private final int id;
+    private final Position position;
     private final Item item;
     private final int xp;
 
-    public Treasure(Item item) {
-        this(item, PICKUP_EXPERIENCE);
+    public Treasure(int id, Position position, Item item) {
+        this(id, position, item, PICKUP_EXPERIENCE);
     }
 
-    protected Treasure(Item item, int xp) {
+    protected Treasure(int id, Position position, Item item, int xp) {
+        this.id = id;
+        this.position = position;
         this.item = item;
         this.xp = xp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Position getPosition() {
+        return position;
     }
 
     public Item getItem() {
